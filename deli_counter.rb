087cbda61ee212865371f4d097903cc02752f65c
Grace Tan <gracetan37@gmail.com>
2019-06-05 15:29:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,6 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
 else
-  katz = []
-  katz_deli.each_with_index do |name,index|
-    katz.push("#{index+1}. #{name} ")
-  return "The line is currently: #{katz}"
-end
-end
+    puts "The line is currently sksjkd"
+  end
 end
