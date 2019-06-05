c5c7c8a3c56f499f1b06b3ae75219798bf7753a4
Grace Tan <gracetan37@gmail.com>
2019-06-05 15:34:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@
 
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.length == 0 
+def line(array)
+  if array.length == 0 
     puts "The line is currently empty."
 else
-  katz_deli.each_with_index do |name,index|
+  array.each_with_index do |name,index|
    puts "#{index+1}. #{name}"
  end
 end
