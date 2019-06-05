ba656760e80a1f6fdeb0c87c701279091ae940a4
Grace Tan <gracetan37@gmail.com>
2019-06-05 15:33:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,8 @@ def line(katz_deli)
     puts "The line is currently empty."
 else
   katz_deli.each_with_index do |name,index|
-   puts "#{index+1}. #{name}""
+   return "#{index+1}. #{name}"
+   
 end
 end
 
