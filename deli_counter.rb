5457e7eedfe9c5bf92a7baf4acb376aa3369725a
Grace Tan <gracetan37@gmail.com>
2019-06-05 15:39:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@ def line(array)
  if array.empty?
    puts "The line is currently empty."
  else 
-   current line = "The line is currently: "
+   current_line = "The line is currently: "
    array.each_with_index(1) do |name,i|
-     current
+     current_line << ""
