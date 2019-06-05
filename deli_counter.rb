36f4fc72815bf1cdde79af9086c70f62028db55c
Grace Tan <gracetan37@gmail.com>
2019-06-05 15:35:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
 else
-  array.each_with_index.map do |name,index|
+  array.each_with_index.map do |name, index|
    puts "#{index+1}. #{name}"
  end
 end
