d091d6d6ad9e27fe2cf94d5d45c5360d93e2c6e0
Grace Tan <gracetan37@gmail.com>
2019-06-05 15:28:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+else
+  katz = []
+  katz_deli.each_with_index do |name,index|
+    katz.push("#{index+1}. #{name} ")
+  end
+  return "The line is currently: #{katz}"
+end
+end
+
